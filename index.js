684038f2556bc24e573ba2cbc8821c4b509910d3
Claire Lesh <cteterita@gmail.com>
2020-07-02 22:21:10 -0700

    improved error handling

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-
 function fetchDogs(breed) {
     fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
         .then(response => response.json())
         .then(parsedResponse => displayDogs(parsedResponse, breed))
-        //.catch(e => alert('Unknown error. Try again later!'));
+        .catch(e => alert(`Error: ${e}`));
 }
 
 function displayDogs(response, breed) {
-    console.log(response);
-    img = response.message;
-    if (img.includes('Breed not found')) {
-        alert ('Dog breed not found. Try something simpler.');
-        return;
+    // Check that the breed was found
+    if (response.code === 404) {
+        throw 'Dog breed not found. Try something simpler.';
     }
 
+    img = response.message;
     $('#image-list').html(`<h2>Behold, the majestic ${breed}</h2>`); // Removes content from previous searches
     $('#image-list').append(`<p><img src='${img}' alt='random ${breed}' width='300'></p>`);
 }
